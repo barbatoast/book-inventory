4f428c255e39e10d39223c350f9369002199a299
barbatoast <christhomas718@gmail.com>
2024-05-18 09:40:52 -0400

    Remove unused function

--- src/api/openLibrary.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const fetchBookDetails = async (isbn: string) => {
-  const response = await axios.get(
-    `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`);
-  return response.data[`ISBN:${isbn}`];
-};
